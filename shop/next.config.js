222ea098e43f1b094b2ef43ba4694b0d8fcf1e3a
hoanghohotel <140063383+hoanghohotel@users.noreply.github.com>
2023-09-28 00:31:45 +0700

    Update next.config.js

--- shop/next.config.js
+++ shop/next.config.js
@@ -13,10 +13,12 @@ module.exports = withPWA({
   i18n,
   images: {
     domains: [
+      'api.mondayui.com',
       'localhost',
       '127.0.0.1:8000',
       'maps.googleapis.com',
       's3.amazonaws.com',
+      'maxui.s3.ap-southeast-1.amazonaws.com',
       'pixarlaravel.s3.ap-southeast-1.amazonaws.com',
       'pickbazarlaravel.s3.ap-southeast-1.amazonaws.com',
     ],
